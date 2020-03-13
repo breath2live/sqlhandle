588f5d09a0c84a5ad85c8818ec0a6d497f12cc16
Sam <infobreath2live@gmail.com>
2020-03-13 14:42:59 +0000

    added package template

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+import os
+import sys
+
+_here = os.path.abspath(os.path.dirname(__file__))
+
+if sys.version_info[0] < 3:
+	print("[Error] Please use python --version greater 3")
+	exit(1)
+
+
+setup(
+	name="sqlhandle",
+	version=open('sqlhandle/version.py').read(),
+	description=('My short description.'),
+	url='https://github.com/breath2live/sqlhandle',
+
+	author='Sam Classix',
+	author_email='sam.classix@example.com',
+	license=open('LICENSE').read(),
+
+	long_description=open('README.md').read(),
+	packages=find_packages(),
+	include_package_data=True,
+
+	#install_requires=['numpy'],
+)
